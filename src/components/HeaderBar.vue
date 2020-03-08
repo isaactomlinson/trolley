1cea36ab800aa61b412cfbd5f8b17f7949aadf36
Isaac Tomlinson <iistornado@gmail.com>
2020-03-08 14:43:38 +1100

    added header component

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app-bar app color="primary" hide-on-scroll="true">
+        <v-toolbar-title>
+            Trolley
+        </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'HeaderBar'
+}
+</script>
+
+<style scoped>
+
+</style>
